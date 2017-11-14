539a2693dabec91d692195cb974dda7cda4ed716
Julien Panopio <jcpanopio1@gmail.com>
2017-11-13 22:44:10 -0800

    Add .gitignore, test /tracks endpoint

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,10 @@ var express = require('express');
 var request = require('request');
 var router = express.Router();
 var qs = require('qs');
+var config = require('../config');
+var clientId = config.clientId;
+var clientSecret = config.clientSecret;
+var redirectUri = config.redirectUri;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -13,8 +17,7 @@ router.get('/helloworld', function(req, res) {
 });
 
 router.get('/spotify-login', function(req, res) {
-  // res.render('spotify-login', { title: 'Spotify Log in' });
-  var scopes = 'user-read-private user-read-email';
+  var scopes = 'user-read-private user-read-email user-library-read';
 
   res.redirect('https://accounts.spotify.com/authorize?' +
     qs.stringify({
@@ -53,8 +56,24 @@ router.get('/spotify-callback', function(req, res) {
 
       // use the access token to access the Spotify Web API
       request.get(options, function(error, response, body) {
-        console.log(body);
+        // console.log(body);
       });
+
+      request.get(
+        {
+          url: 'https://api.spotify.com/v1/me/tracks',
+          headers: { 'Authorization': 'Bearer ' + accessToken },
+          json: true
+        },
+        function(error, response, body) {
+          var track = body.items[0].track;
+          var trackName = track.name;
+          var trackArtist = track.artists[0].name;
+          var trackImage = track.album.images[0].url;
+
+          console.log(trackName, trackArtist, trackImage);
+        },
+      );
 
       // we can also pass the token to the browser to make requests from there
       res.redirect('/#' +
